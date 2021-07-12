e061ca8852c707553e0d2a5c3b10cf260ec98f7d
skitarii01 <defstrouk2014@yandex.ru>
2021-07-12 13:25:36 +0300

    нарастил мясо. Сделал класс для работы с базами sqlite. Начал делать интерфейс

--- Data.py
+++ Data.py
@@ -4,36 +4,35 @@ import sqlite3 as sql
 class Data(object):
 
     # логирование нужно или нет?
-    def __init__(self):
+    def __init__(self, user_id):
+        self.user_id = user_id
         pass
 
-    @staticmethod
-    def add_leaf(name, description, difficulty, prioritet, parent=10000):
+    def add_leaf(self, name, description, difficulty, prioritet, parent=10000):
         # добавляет в базу tree новый лист с новым id
 
-        conn = sql.connect('tree.db')
+        conn = sql.connect('trees/%s.db' % (self.user_id))
         cur = conn.cursor()
         new_id = cur.execute("SELECT * FROM tree;").fetchall()[-1][0] + 1
         parameters = (new_id, parent, name, description, difficulty, prioritet)
         cur.execute("""INSERT INTO tree VALUES(?, ?, ?, ?, ?, ?);""", parameters)
         conn.commit()
 
-    @staticmethod
-    def delete_leaf(leaf_id):
-        conn = sql.connect('tree.db')
+    def delete_leaf(self, leaf_id):
+        conn = sql.connect('trees/%s.db' % (self.user_id))
         cur = conn.cursor()
         parameters = tuple([leaf_id])
         cur.execute("DELETE FROM tree WHERE ID = ?;", parameters)
         conn.commit()
 
-    @staticmethod
-    def get_leaf(leaf_id):
-        conn = sql.connect('tree.db')
+    def get_leaf(self, leaf_id):
+        conn = sql.connect('trees/%s.db' % (self.user_id))
         cur = conn.cursor()
         last_data = cur.execute("SELECT * FROM tree;").fetchall()
         for leaf in last_data:
             if leaf[0] == leaf_id:
                 return leaf
+
 
 # нужен ли этот класс?
 '''class Leaf(object):
