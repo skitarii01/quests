8fc681f19b2861ae0a65f17bd3e45dc342fb2014
skitarii01 <defstrouk2014@yandex.ru>
2021-07-22 04:35:59 +0300

    big upd

--- keyboards.py
+++ keyboards.py
@@ -3,5 +3,6 @@ from telebot import types
 main_kb = types.ReplyKeyboardMarkup(resize_keyboard=2)
 main_kb.row(types.KeyboardButton('квесты'), types.KeyboardButton('текущие квесты'), types.KeyboardButton('статистика'))
 
+'''
 quest_kb = types.InlineKeyboardMarkup()
-quest_kb.add(types.InlineKeyboardButton(text='изменить инфу',callback_data='edit_info'), types.InlineKeyboardButton(text='добавить квест', callback_data='add_new_quest'))
+quest_kb.add(types.InlineKeyboardButton(text='изменить инфу',callback_data='edit_info'), types.InlineKeyboardButton(text='добавить квест', callback_data='add_new_quest'))'''
